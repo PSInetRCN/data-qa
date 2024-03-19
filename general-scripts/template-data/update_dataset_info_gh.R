0cca5ddd2a7019d91639bfff7359ce76cb1556a8
Renata Diaz <renatadiaz@arizona.edu>
2024-03-19 09:15:09 -0700

    add dataset_tracking and reorganize files with clearer names

---
+++ general-scripts/template-data/update_dataset_info_gh.R
@@ -0,0 +1,16 @@
+library(pins)
+library(boxr)
+library(dplyr)
+
+source(here::here("general-scripts", "box_auth.R"))
+
+info_data_box <- box_read_excel("1475679138432", sheet = 1)
+
+info_data_local <- read.csv(here::here("dataset_tracking.csv"))
+
+new_box <- info_data_box |>
+  filter(!(response_ID %in% info_data_local$response_ID))
+
+info_data_local <- bind_rows(info_data_local, new_box)
+
+write.csv(info_data_local, here::here("dataset_tracking.csv"), row.names = FALSE)
